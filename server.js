8185089c294e74a12a26577b69aa8344483bf36b
themadtaco <aatrox48@gmail.com>
2022-02-15 18:19:37 -0600

    connected server to mysql

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password,
+        password: 'maxdog48',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+})
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
